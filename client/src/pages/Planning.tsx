d6d4125df86633907b17aa6c727c9806bd36417e
seyun00 <reny83@naver.com>
2025-05-23 19:08:00 +0900

    csy_네이버지도 api 환경 설정

--- client/src/pages/Planning.tsx
+++ client/src/pages/Planning.tsx
@@ -1,12 +1,36 @@
 import { Link } from "react-router-dom"
+import { useEffect, useRef } from 'react';
 
 export default function Planning() {
+  const mapRef = useRef(null);
+  useEffect(() => {
+    const { naver } = window;
+    if (mapRef.current && naver) {
+      const location = new naver.maps.LatLng(37.5665, 126.9780); // 예시: 서울시청
+      const mapOptions = {
+        center: location,
+        zoom: 15,
+        zoomControl: true,
+      };
+      const map = new naver.maps.Map(mapRef.current, mapOptions);
+
+      // 마커 추가 예시
+      new naver.maps.Marker({
+        position: location,
+        map,
+      });
+    }
+  }, []);
   return (
     <> 
       <h1>여행 계획 페이지</h1>
       <Link to='/'>
         <button>홈으로</button>
       </Link>
+      <div
+      ref={mapRef}
+      style={{ width: '100%', height: '400px' }}
+    />
     </>
   )
 }
